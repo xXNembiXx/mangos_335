eccbee7e8b1a6d197cb0f4b2ed55da613361df38
KeRo99 <motrilwireless@gmail.com>
2010-10-14 20:37:03 +0200

    Revert "[K25] Implementation of Spell Disabling System"
    This reverts commit 15540692c0265abc4d5cf16de1f72c5e0f6d9e68.

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -445,7 +445,6 @@ class ChatHandler
         bool HandleReloadSpellTargetPositionCommand(char* args);
         bool HandleReloadSpellThreatsCommand(char* args);
         bool HandleReloadSpellPetAurasCommand(char* args);
-        bool HandleReloadSpellDisabledCommand(char* args);
 
         bool HandleResetAchievementsCommand(char* args);
         bool HandleResetAllCommand(char* args);
--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -168,7 +168,6 @@ bool ChatHandler::HandleReloadAllSpellCommand(char* /*args*/)
     HandleReloadSpellTargetPositionCommand((char*)"a");
     HandleReloadSpellThreatsCommand((char*)"a");
     HandleReloadSpellPetAurasCommand((char*)"a");
-    HandleReloadSpellDisabledCommand("a");
     return true;
 }
 
@@ -905,17 +904,6 @@ bool ChatHandler::HandleReloadMailLevelRewardCommand(char* /*args*/)
     sLog.outString( "Re-Loading Player level dependent mail rewards..." );
     sObjectMgr.LoadMailLevelRewards();
     SendGlobalSysMessage("DB table `mail_level_reward` reloaded.");
-    return true;
-}
-
-bool ChatHandler::HandleReloadSpellDisabledCommand(char* /*arg*/)
-{
-    sLog.outString( "Re-Loading spell disabled table...");
-
-    sObjectMgr.LoadSpellDisabledEntrys();
-
-    SendGlobalSysMessage("DB table `spell_disabled` reloaded.");
-
     return true;
 }
 
